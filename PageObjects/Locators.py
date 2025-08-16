d55a8b22e960c2ba4c921f519a80151fdd67247b
arpitgarg09bit <arpitgarg9897@gmail.com>
2025-08-16 17:52:35 +0530

    Add files via upload

---
+++ PageObjects/Locators.py
@@ -0,0 +1,6 @@
+#Login Page elements
+txt_Username="xpath://input[@placeholder='Username']"
+txt_Password="xpath://input[@placeholder='Password']"
+btn_Login="xpath://button[normalize-space()='Login']"
+
+
